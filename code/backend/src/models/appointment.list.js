a76cee1a1c88ade7699ce405d72f110e00c2a650
vitofevrier <vitofev15202@gmail.com>
2024-05-15 12:31:33 +0700

    [BACKEND] List appointment

---
+++ code/backend/src/models/appointment.list.js
@@ -0,0 +1,70 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+function generateWhere(type, user) {
+  if (type === '') {
+    return {
+      OR: [
+        {
+          organizerId: {
+            equals: user.id,
+          },
+        },
+        {
+          participantId: {
+            equals: user.id,
+          },
+        },
+      ],
+    };
+  }
+
+  if (type === 'participant') {
+    return {
+      participantId: user.id,
+    };
+  }
+
+  return {
+    organizerId: user.id,
+  };
+}
+
+async function listAppointment(args) {
+  const {
+    locals: { user },
+    query: requestQuery,
+  } = args;
+
+  try {
+    const appointsments = await prisma.appointment.findMany({
+      where: generateWhere(requestQuery.type, user),
+      select: {
+        id: true,
+        topic: true,
+        date: true,
+        startTime: true,
+        endTime: true,
+        organizer: {
+          select: {
+            name: true,
+          },
+        },
+        participant: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    const payload = appointsments;
+
+    return payload;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export { listAppointment };
--- code/backend/src/services/appointment.service.js
+++ code/backend/src/services/appointment.service.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { createAppointment } from '../models/appointment.create.js';
+import { listAppointment } from '../models/appointment.list.js';
 import { verify } from '../models/authentication.js';
 
 const AppointmentService = express.Router();
@@ -7,6 +8,15 @@ const AppointmentService = express.Router();
 AppointmentService.post('/', verify('all'), async (req, res, next) => {
   try {
     const result = await createAppointment(req);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+AppointmentService.get('/', verify('all'), async (req, res, next) => {
+  try {
+    const result = await listAppointment(req);
     res.json(result);
   } catch (error) {
     next(error);
